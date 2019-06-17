51188fcbbceddeac92d3928c43cbc20793f58f9d
usava <uptofly73@gmail.com>
2019-06-17 05:22:35 +0300

    [FEATURE] Thread, Reply models, migrations

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,8 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 use App\User;
+use App\Thread;
+use App\Reply;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
@@ -23,5 +25,27 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+    ];
+});
+
+$factory->define(Thread::class, function ($faker){
+    return [
+        'user_id' => function() {
+            return factory(User::class)->create()->id;
+        },
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph
+    ];
+});
+
+$factory->define(Reply::class, function ($faker){
+    return [
+        'thread_id' => function() {
+            return factory(Thread::class)->create()->id;
+        },
+        'user_id' => function() {
+            return factory(User::class)->create()->id;
+        },
+        'body' => $faker->paragraph
     ];
 });
